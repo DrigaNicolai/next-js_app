d2d4ae3b868fccd1469701f98f30675644bbe874
DrigaNicolai <just.whyme7@gmail.com>
2023-09-05 17:16:33 +0300

    Modal dialog with report form and logic

---
+++ components/ReportForm.jsx
@@ -0,0 +1,34 @@
+const ReportForm = ({ message, setMessage, submitting, handleSubmit }) => {
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+    >
+      <label>
+        <span className="font-satoshi font-semibold text-base text-gray-700">
+          Your comment
+        </span>
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Write your comment here..."
+          required
+          className="form_textarea"
+        />
+      </label>
+
+      <button
+        type="submit"
+        disabled={submitting}
+        className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+      >
+        { submitting
+          ? "Submitting"
+          : "Submit"
+        }
+      </button>
+    </form>
+  )
+};
+
+export default ReportForm;
