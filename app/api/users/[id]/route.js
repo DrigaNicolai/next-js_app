6a2aa49a8550bd4beb4b6286feeb3e2735a0f9a6
DrigaNicolai <just.whyme7@gmail.com>
2023-09-17 21:22:24 +0300

    Update get user method

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -1,5 +1,22 @@
 import { connectToDB } from "@utils/database";
 import User from "@models/user";
+import Role from "@models/role";
+
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    const user = await User.findById(params.id).populate("role_id");
+
+    if (!user) {
+      return new Response(JSON.stringify({ message: "User not found" }), { status: 404 });
+    }
+
+    return new Response(JSON.stringify(user), { status: 200 });
+  } catch (error) {
+    return new Response(`Failed to fetch user: ${error.message}`, { status: 500 });
+  }
+}
 
 export const DELETE =  async (req, { params }) => {
   try {
