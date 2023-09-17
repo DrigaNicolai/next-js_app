6a12f9ccb21893cdbd7aa9f21df4d69ade4f5ae5
DrigaNicolai <just.whyme7@gmail.com>
2023-09-17 21:36:41 +0300

    Middleware

---
+++ middleware/useUserRole.js
@@ -0,0 +1,29 @@
+import { useSession } from "next-auth/react";
+
+export function useUserRole(roles) {
+  const { data: session } = useSession();
+
+  /*useEffect(() => {
+    const getRole = async () => {
+      if (session) {
+        const userRole = await getUserRole(session?.user.id);
+
+        /!*userRole.then((res) => setUserRoleName(res))
+          .catch(() => { console.log("Error to parse user role") });*!/
+        setUserRoleName(userRole);
+      }
+    }
+
+    getRole();
+  }, [session]);*/
+
+  if (!session) {
+    return null;
+  }
+
+  if (!roles.includes(session?.user.role)) {
+    return null;
+  }
+
+  return session?.user.role;
+}
