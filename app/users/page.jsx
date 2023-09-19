725b9d540e061165c35c0a606156a0c1a3c79773
NicolaiDriga <just.whyme7@gmail.com>
2023-09-19 12:54:01 +0300

    Create DataTable component WIP

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { useUserRole } from "@middleware/useUserRole";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import DataTable from "@components/DataTable";
 
 const Users = () => {
   const user = useUserRole(["admin", "user"]); // TODO: Remove user
   const router = useRouter();
   const { data: session } = useSession();
+  const [users, setUsers] = useState([]);
+  const [headers, setHeaders] = useState([
+    {text: "Name", value: "name"},
+    {text: "Email", value: "email"},
+    {text: "Role", value: "role"},
+  ]);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -19,9 +26,9 @@ const Users = () => {
         }
       });
 
-      // const data = await response.json();
+      const data = await response.json();
 
-      // console.log(data);
+      setUsers(data);
     }
 
     if (!user) {
@@ -31,9 +38,108 @@ const Users = () => {
     fetchUsers();
   }, [user]);
 
+  const test = () => {
+    console.log("test")
+  }
+
   return (
-    <div>
+    <div className="w-full max-w-full flex-start flex-col">
       USERS TEST
+      {/*<div className="flex flex-col mt-10 w-full max-w-2xl gap-7">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full text-left text-sm font-light">
+                <thead className="border-b font-medium dark:border-neutral-500">
+                <tr>
+                  <th scope="col" className="px-6 py-4">#</th>
+                  <th scope="col" className="px-6 py-4">First</th>
+                  <th scope="col" className="px-6 py-4">Last</th>
+                  <th scope="col" className="px-6 py-4">Handle</th>
+                  <th scope="col" className="px-6 py-4">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr className="border-b dark:border-neutral-500">
+                  <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                  <td className="whitespace-nowrap px-6 py-4">Mark</td>
+                  <td className="whitespace-nowrap px-6 py-4">Otto</td>
+                  <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                </tr>
+                <tr className="border-b dark:border-neutral-500">
+                  <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
+                  <td className="whitespace-nowrap px-6 py-4">Jacob</td>
+                  <td className="whitespace-nowrap px-6 py-4">Thornton</td>
+                  <td className="whitespace-nowrap px-6 py-4">@fat</td>
+                </tr>
+                <tr className="border-b dark:border-neutral-500">
+                  <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
+                  <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                  <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                  <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>*/}
+      {/*<div className="flex items-center justify-center">
+        <div className="container">
+          <table className="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+            <thead className="text-white">
+            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+              <th className="p-3 text-left">Name</th>
+              <th className="p-3 text-left">Email</th>
+              <th className="p-3 text-left" width="110px">Actions</th>
+            </tr>
+            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+              <th className="p-3 text-left">Name</th>
+              <th className="p-3 text-left">Email</th>
+              <th className="p-3 text-left" width="110px">Actions</th>
+            </tr>
+            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+              <th className="p-3 text-left">Name</th>
+              <th className="p-3 text-left">Email</th>
+              <th className="p-3 text-left" width="110px">Actions</th>
+            </tr>
+            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+              <th className="p-3 text-left">Name</th>
+              <th className="p-3 text-left">Email</th>
+              <th className="p-3 text-left" width="110px">Actions</th>
+            </tr>
+            </thead>
+            <tbody className="flex-1 sm:flex-none">
+            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+              <td className="border-grey-light border hover:bg-gray-100 p-3">John Covv</td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">contato@johncovv.com</td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+            </tr>
+            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+              <td className="border-grey-light border hover:bg-gray-100 p-3">Michael Jackson</td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">m_jackson@mail.com</td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+            </tr>
+            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+              <td className="border-grey-light border hover:bg-gray-100 p-3">Julia</td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">julia@mail.com</td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+            </tr>
+            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+              <td className="border-grey-light border hover:bg-gray-100 p-3">Martin Madrazo</td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">martin.madrazo@mail.com</td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>*/}
+      <DataTable
+        data={users}
+        headers={headers}
+        handleEdit={test}
+        handleDelete={test}
+      />
     </div>
   );
 };
