ebd24ac236a63701d70d1cdc4c3d020398a703dc
NicolaiDriga <just.whyme7@gmail.com>
2023-09-21 11:13:18 +0300

    Users datatable render (WIP)

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -23,21 +23,25 @@ const Users = () => {
       const response = await fetch(`/api/users`,{
         method: "GET",
         headers: {
-          "Authorization": `Bearer ${session.token}`
+          "Authorization": `Bearer ${session?.token}`
         }
       });
 
       const data = await response.json();
 
-      setUsers(data);
+      const parsedData = data.map((item) => (
+        { id: item._id, email: item.email, username: item.username, role: item.role_id.name }
+      ));
+
+      setUsers(parsedData);
     }
 
     const fetchHeaders = (): void => {
       const response = getTableHeaders("users");
 
-      const parsedHeaders = [...response, { text: "Actions", value: "actions", }];
+      // const parsedHeaders = [...response, { text: "Actions", value: "actions", }];
 
-      setHeaders(parsedHeaders);
+      setHeaders(response);
     }
 
     if (!user) {
@@ -54,7 +58,9 @@ const Users = () => {
 
   return (
     <div className="w-full max-w-full flex-start flex-col">
-      USERS TEST
+      <h1 className='head_text text-left'>
+        <span className="blue_gradient">Users table</span>
+      </h1>
       {/*<div className="flex flex-col mt-10 w-full max-w-2xl gap-7">
         <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
@@ -147,8 +153,6 @@ const Users = () => {
       <DataTable
         data={users}
         headers={headers}
-        handleEdit={test}
-        handleDelete={test}
       />
     </div>
   );
