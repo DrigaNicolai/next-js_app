85bf498c50c762f2b6616764540a2d2286ac6561
NicolaiDriga <just.whyme7@gmail.com>
2023-09-21 09:27:19 +0300

    Parse headers (WIP)

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -7,29 +7,15 @@ interface IDataTable {
 
 const DataTable = ({ headers, data, handleDelete, handleEdit }: IDataTable) => {
   return (
-    <div className="flex items-center justify-center">
+    <section className="flex items-center justify-center">
       <div className="container">
         <table className="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
           <thead className="text-white">
-            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
-              <th className="p-3 text-left">Name</th>
-              <th className="p-3 text-left">Email</th>
-              <th className="p-3 text-left" style={{ width: '110px' }}>Actions</th>
-            </tr>
-            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
-              <th className="p-3 text-left">Name</th>
-              <th className="p-3 text-left">Email</th>
-              <th className="p-3 text-left" style={{ width: '110px' }}>Actions</th>
-            </tr>
-            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
-              <th className="p-3 text-left">Name</th>
-              <th className="p-3 text-left">Email</th>
-              <th className="p-3 text-left" style={{ width: '110px' }}>Actions</th>
-            </tr>
-            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
-              <th className="p-3 text-left">Name</th>
-              <th className="p-3 text-left">Email</th>
-              <th className="p-3 text-left" style={{ width: '110px' }}>Actions</th>
+            <tr className="bg-orange-300 flex flex-col flex-nowrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+              { headers.map(header => (
+                  <th key={header.value} className="p-3 text-left">{header.text}</th>
+                )
+              )}
             </tr>
           </thead>
           <tbody className="flex-1 sm:flex-none">
@@ -53,14 +39,14 @@ const DataTable = ({ headers, data, handleDelete, handleEdit }: IDataTable) => {
               <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">martin.madrazo@mail.com</td>
               <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
             </tr>*/}
-            { data.map((item) => (
-                <div key={item._id}>{item._id}</div>
+            {/*{ data.map((item) => (
+                <div key={item._id}>{item._id} - {item['username']}</div>
               )
-            )}
+            )}*/}
           </tbody>
         </table>
       </div>
-    </div>
+    </section>
   );
 };
 
