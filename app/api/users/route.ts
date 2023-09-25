1ef7c066f92d811af72c53a8bdcd4e8ea37641c4
DrigaNicolai <just.whyme7@gmail.com>
2023-09-25 21:11:34 +0300

    Update api/users

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,33 +1,9 @@
-import { connectToDB } from "@utils/database";
 import User from "@models/user";
-import Role from "@models/role";
-import jwt from "jsonwebtoken";
-import { headers } from "next/headers";
-import { getAvailableRoles } from "@static/getAvailableRoles";
-import {IUser} from "@ts/interface/user";
+import { checkCredentials } from "@utils/checkCredentials";
 
 export const GET = async (req, res): Promise<Response> => {
   try {
-    await connectToDB();
-
-    const headersInstance = headers()
-    const authorization = headersInstance.get('authorization')
-
-    const token = authorization.split(" ")[1];
-
-    if (!token) {
-      return new Response(JSON.stringify({ message: "User is not authenticated" }), { status: 401 });
-    }
-
-    const { role: userRole } = jwt.verify(token, process.env.JWT_SECRET) as IUser;
-
-    const permittedRoles = getAvailableRoles("getUsers");
-
-    const hasPermission = permittedRoles.includes(userRole);
-
-    if (!hasPermission) {
-      return new Response(JSON.stringify({ message: "Permission denied" }), { status: 403 });
-    }
+    await checkCredentials("getUsers");
 
     const users = await User.find({}).populate("role_id");
 
