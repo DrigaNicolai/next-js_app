3db843eebb3280a3838d8487498cd00882e0fe63
DrigaNicolai <just.whyme7@gmail.com>
2023-09-25 21:12:50 +0300

    Update api/users/[id]

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,6 +1,7 @@
 import { connectToDB } from "@utils/database";
 import User from "@models/user";
 import Role from "@models/role";
+import { checkCredentials } from "@utils/checkCredentials";
 
 export const GET = async (req, { params }): Promise<Response> => {
   try {
@@ -18,21 +19,42 @@ export const GET = async (req, { params }): Promise<Response> => {
   }
 }
 
+export const PATCH = async (req, { params }): Promise<Response> => {
+  const { role_id } = await req.json();
+
+  try {
+    await checkCredentials("updateUser");
+
+    const existingUser = await User.findById(params.id);
+
+    if (!existingUser) {
+      return new Response(JSON.stringify({ message: "User not found" }), { status: 404 });
+    }
+
+    await User.updateOne({ _id: params.id }, { role_id });
+
+    return new Response(JSON.stringify({ message: "User successfully edited" }), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ message: `Error updating user, ${error}` }), { status: 400 });
+  }
+}
+
 export const DELETE =  async (req, { params }): Promise<Response> => {
   try {
-    await connectToDB();
+    await checkCredentials("deleteUser");
 
     const user = await User.findById(params.id);
 
+
     if (!user) {
-      return new Response("Prompt not found", { status: 404 });
+      return new Response(JSON.stringify({ message: "User not found" }), { status: 404 });
     }
 
     // @ts-ignore
     await User.deleteOne(user);
 
-    return new Response("Prompt deleted successfully", { status: 200 });
+    return new Response(JSON.stringify({ message: "User deleted successfully" }), { status: 200 });
   } catch (error) {
-    return new Response("Error Updating Prompt", { status: 500 });
+    return new Response("Error Deleting User", { status: 500 });
   }
 }
