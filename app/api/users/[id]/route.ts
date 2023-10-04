4aacdd0bb58382f540ee077cab81cb3ef53a4157
DrigaNicolai <just.whyme7@gmail.com>
2023-10-04 21:48:23 +0300

    Update users routes

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,60 +1,20 @@
-import { connectToDB } from "@utils/database";
-import User from "@models/user";
-import Role from "@models/role";
-import { checkCredentials } from "@utils/checkCredentials";
+import userController from "@controllers";
 
 export const GET = async (req, { params }): Promise<Response> => {
-  try {
-    await connectToDB();
+  const { status, response } = await userController.userController().getUser(params.id);
 
-    const user = await User.findById(params.id).populate("role_id");
-
-    if (!user) {
-      return new Response(JSON.stringify({ message: "User not found" }), { status: 404 });
-    }
-
-    return new Response(JSON.stringify(user), { status: 200 });
-  } catch (error) {
-    return new Response(`Failed to fetch user: ${error.message}`, { status: 500 });
-  }
+  return new Response(JSON.stringify(response), { status });
 }
 
 export const PATCH = async (req, { params }): Promise<Response> => {
   const { role_id } = await req.json();
+  const { status, response } = await userController.userController().updateUser(params.id, { role_id });
 
-  try {
-    await checkCredentials("updateUser");
-
-    const existingUser = await User.findById(params.id);
-
-    if (!existingUser) {
-      return new Response(JSON.stringify({ message: "User not found" }), { status: 404 });
-    }
-
-    await User.updateOne({ _id: params.id }, { role_id });
-
-    return new Response(JSON.stringify({ message: "User successfully edited" }), { status: 200 });
-  } catch (error) {
-    return new Response(JSON.stringify({ message: `Error updating user, ${error}` }), { status: 400 });
-  }
+  return new Response(JSON.stringify(response), { status });
 }
 
 export const DELETE =  async (req, { params }): Promise<Response> => {
-  try {
-    await checkCredentials("deleteUser");
+  const { status, response } = await userController.userController().deleteUser(params.id);
 
-    const user = await User.findById(params.id);
-
-
-    if (!user) {
-      return new Response(JSON.stringify({ message: "User not found" }), { status: 404 });
-    }
-
-    // @ts-ignore
-    await User.deleteOne(user);
-
-    return new Response(JSON.stringify({ message: "User deleted successfully" }), { status: 200 });
-  } catch (error) {
-    return new Response("Error Deleting User", { status: 500 });
-  }
+  return new Response(JSON.stringify(response), { status });
 }
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,7 @@
 import userController from "@controllers/index";
 
 export const GET = async (req, res): Promise<Response> => {
-  const { status, response } = await userController.userController().getUsers();
+  const { status, response } = await userController.userController().getAllUsers();
 
   return new Response(JSON.stringify(response), { status });
 }
