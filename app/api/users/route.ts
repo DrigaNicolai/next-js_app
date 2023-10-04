6b8022109e9cda2f1f5dd15aeadaab0ae9fe2a45
DrigaNicolai <just.whyme7@gmail.com>
2023-10-04 20:07:43 +0300

    Change api/users/route file

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,20 +1,7 @@
-import { checkCredentials } from "@utils/checkCredentials";
 import userController from "@controllers/index";
 
 export const GET = async (req, res): Promise<Response> => {
-  try {
-    await checkCredentials("getUsers");
+  const { status, response } = await userController.userController().getUsers();
 
-    const { status, response } = await userController.userController().getUsers();
-
-    // const { data, status } = await getUsers();
-
-    /*if (!users) {
-      return new Response(JSON.stringify({ message: "Users not found" }), { status: 404 });
-    }*/
-
-    return new Response(JSON.stringify(response), { status });
-  } catch (error) {
-    return new Response(`Failed to fetch all users ${JSON.stringify(error.message)}`, { status: 500 });
-  }
+  return new Response(JSON.stringify(response), { status });
 }
