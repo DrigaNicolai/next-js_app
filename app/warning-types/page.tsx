352acfb514ace7a38d3aad6935866523d77ec36c
NicolaiDriga <just.whyme7@gmail.com>
2023-10-06 09:03:06 +0300

    Warning types. Add to navbar, create page (WIP)

---
+++ app/warning-types/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+const Page = () => {
+  return (
+    <div className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">
+          Waning Types table
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
