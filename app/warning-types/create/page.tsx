63b4b2cb77b222cacc50faf48ee33ba04868f057
DrigaNicolai <just.whyme7@gmail.com>
2023-10-07 17:51:35 +0300

    WarningType/create page

--- app/warning-types/create/page.tsx
+++ app/warning-types/create/page.tsx
@@ -1,10 +1,53 @@
 "use client";
 
+import React, { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { AppRouterInstance } from "@node_modules/next/dist/shared/lib/app-router-context";
+
+import WarningTypeForm from "@components/form/WarningTypeForm";
+import CustomSession from "@ts/interface/customAuth";
+import { IWarningType } from "@ts/interface/warningType";
+
+
 const CreateWarningType = () => {
+  const router = useRouter() as AppRouterInstance;
+  const { data: session } = useSession() as unknown as CustomSession;
+
+  const [submitting, setSubmitting] = useState(false as boolean);
+  const [warningType, setWarningType] = useState({} as IWarningType)
+
+  const createWarningType = async (e: React.MouseEvent): Promise<void> => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/warning-types/create", {
+        method: "POST",
+        body: JSON.stringify(warningType),
+        headers: {
+          "Authorization": `Bearer ${session?.token}`
+        }
+      });
+
+      if (response.ok) {
+        router.push("/warning-types");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
   return (
-    <div>
-      Create warning type
-    </div>
+    <WarningTypeForm
+      type="Create"
+      warningType={warningType}
+      setWarningType={setWarningType}
+      submitting={submitting}
+      handleSubmit={createWarningType}
+    />
   );
 };
 
