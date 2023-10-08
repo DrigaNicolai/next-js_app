3752c3b099694de8a2e5c1953d7cfc548abcc91e
DrigaNicolai <just.whyme7@gmail.com>
2023-10-08 12:40:45 +0300

    Add getOne, update and delete methods for warning type service

--- services/WarningTypeService.ts
+++ services/WarningTypeService.ts
@@ -1,4 +1,6 @@
 import WarningType from "@models/warningType";
+import mongoose from "mongoose";
+import {IWarningType} from "@ts/interface/warningType";
 
 export default class WarningTypeService {
   async getAllWarningTypes(): Promise<any> {
@@ -7,5 +9,17 @@ export default class WarningTypeService {
 
   async create(body: object): Promise<any> {
     await WarningType.create(body);
+  }
+
+  async getWarningType(id: string): Promise<any> {
+    return WarningType.findById(id);
+  }
+
+  async updateWarningType(id: string, payload: object): Promise<any> {
+    await WarningType.updateOne({ _id: id }, payload);
+  }
+
+  async deleteWarningType(warningType: mongoose.Model<IWarningType>): Promise<any> {
+    return WarningType.deleteOne(warningType);
   }
 }
