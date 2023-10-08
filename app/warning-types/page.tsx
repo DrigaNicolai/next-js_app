15c585a443c2a285e6463ce5595aaeb95f7b5b74
DrigaNicolai <just.whyme7@gmail.com>
2023-10-08 12:40:19 +0300

    Update main warning type page with new methods and render logic

--- app/warning-types/page.tsx
+++ app/warning-types/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useUserRole } from "@middleware/useUserRole";
 import { useRouter } from "next/navigation";
 import { AppRouterInstance } from "@node_modules/next/dist/shared/lib/app-router-context";
 import { useSession } from "next-auth/react";
@@ -12,7 +13,7 @@ import DataTable from "@components/DataTable";
 import Link from "next/link";
 
 const Page = () => {
-  // const user = useUserRole(["admin", "moderator", "user"]) as string;
+  const user = useUserRole(["admin", "moderator", "user"]) as string;
   const router: AppRouterInstance = useRouter();
   const { data: session } = useSession() as unknown as CustomSession
   const [warningTypes, setWarningTypes] = useState([] as Array<IWarningType>);
@@ -39,6 +40,35 @@ const Page = () => {
     fetchHeaders();
   }, []);
 
+  const handleEdit = (id: string): void => {
+    router.push(`/warning-types/${id}/edit`);
+  }
+
+  const handleDelete = async (warningType: IWarningType): Promise<void> => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this warning type?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        const response = await fetch(`/api/warning-types/${warningType._id.toString()}`, {
+          method: "DELETE",
+          headers: {
+            "Authorization": `Bearer ${session?.token}`
+          }
+        });
+
+        if (response.ok) {
+          const filteredWarningTypes = warningTypes.filter((item) => item._id !== warningType._id);
+
+          setWarningTypes(filteredWarningTypes);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <div className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
@@ -46,20 +76,30 @@ const Page = () => {
           Warning Types table
         </span>
       </h1>
-      <Link href="/warning-types/create" className="black_btn mt-2">
-        Create warning type
-      </Link>
-      {!warningTypes.length ? (
-        <p className="desc text-left">There are no warning types</p>
+      {user === "admin" ? (
+        <Link href="/warning-types/create" className="black_btn mt-2">
+          Create warning type
+        </Link>
       ) : (
-        <DataTable
-          data={warningTypes}
-          headers={headers}
-        />
+        <></>
       )}
 
-      { JSON.stringify(headers) }
-      { JSON.stringify(warningTypes) }
+      {!warningTypes.length ? (
+        <p className="desc text-left">There are no warning types</p>
+      ) : (user === "admin" ? (
+          <DataTable
+            data={warningTypes}
+            headers={headers}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
+        ) : (
+          <DataTable
+            data={warningTypes}
+            headers={headers}
+          />
+        )
+      )}
     </div>
   );
 };
