c554d237fc1c3ab26ac7eb52a69d4f1899f9d09c
DrigaNicolai <just.whyme7@gmail.com>
2023-11-07 16:06:50 +0200

    Create tag applications service

--- services/index.ts
+++ services/index.ts
@@ -1,9 +1,11 @@
 import UserService from "@services/UserService";
 import WarningTypeService from "@services/WarningTypeService";
 import TagService from "@services/TagService";
+import TagApplicationService from "@services/TagApplicationService";
 
 export default {
   userService: () => new UserService(),
   warningTypeService: () => new WarningTypeService(),
-  tagService: () => new TagService()
+  tagService: () => new TagService(),
+  tagApplicationService: () => new TagApplicationService()
 }
