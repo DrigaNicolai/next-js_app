7f4ad75dcc8069d496d2f09ec9905897cb170c7d
DrigaNicolai <just.whyme7@gmail.com>
2023-11-15 12:51:14 +0200

    Create report controller

--- controllers/index.ts
+++ controllers/index.ts
@@ -3,11 +3,13 @@ import WarningTypeController from "@controllers/WarningTypeController";
 import TagController from "@controllers/TagController";
 import TagApplicationController from "@controllers/TagApplicationController";
 import PostController from "@controllers/PostController";
+import ReportController from "@controllers/ReportController";
 
 export default {
   userController: () => new UserController(),
   warningTypeController: () => new WarningTypeController(),
   tagController: () => new TagController(),
   tagApplicationController: () => new TagApplicationController(),
-  postController: () => new PostController()
+  postController: () => new PostController(),
+  reportController: () => new ReportController()
 }
