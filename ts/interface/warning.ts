ef96dc3e672eae1df1f3152e86c5fe2b7fdd5df1
DrigaNicolai <just.whyme7@gmail.com>
2023-11-16 16:03:22 +0200

    Update warning interface

--- ts/interface/warning.ts
+++ ts/interface/warning.ts
@@ -2,9 +2,12 @@ import { IUser } from "@ts/interface/user";
 import { IWarningType } from "@ts/interface/warningType";
 
 export interface IWarning {
-  _id: string | number,
-  moderator_id: IUser;
-  intruder_id: IUser;
-  warning_type_id: IWarningType;
+  _id?: string | number,
+  moderator_id?: IUser | string | any;
+  intruder_id: IUser | string | any;
+  warning_type_id: IWarningType | string | any;
   comment: string;
+  moderator_name?: string;
+  intruder_name?: string;
+  warning_type_name?: string;
 }
