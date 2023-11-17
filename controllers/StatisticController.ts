37a157b6f2ec86b93650f72383c715d819f85a16
DrigaNicolai <just.whyme7@gmail.com>
2023-11-17 17:19:57 +0200

    Update StatisticController with user statistics method

--- controllers/StatisticController.ts
+++ controllers/StatisticController.ts
@@ -1,6 +1,7 @@
 import { IResponse } from "@ts/interface/global";
 import userService from "@services";
 import postService from "@services";
+import warningService from "@services";
 
 export default class StatisticController {
   async getGlobalStatistics(): Promise<IResponse> {
@@ -35,6 +36,47 @@ export default class StatisticController {
       return {
         status: 200,
         response: { total_users, total_posts, frequent_tags }
+      }
+    } catch (error) {
+      return {
+        status: 500,
+        response: { message: `Failed to fetch statistics ${error.message}` }
+      }
+    }
+  }
+
+  async getProfileStatistics(id: string): Promise<IResponse> {
+    try {
+      const total_posts = await postService.postService().getTotalUserPosts(id);
+
+      if (!total_posts) {
+        return {
+          status: 422,
+          response: { message: "User's posts can not be counted" }
+        }
+      }
+
+      const [favorite_tag] = await userService.userService().getUserFrequentTags(id,1);
+
+      if (!favorite_tag) {
+        return {
+          status: 422,
+          response: { message: "User's frequent tags can not be found" }
+        }
+      }
+
+      const [warning_points] = await warningService.warningService().getUserWarningPoints(id);
+
+      if (!warning_points) {
+        return {
+          status: 422,
+          response: { message: "User's warning point can not be calculated" }
+        }
+      }
+
+      return  {
+        status: 200,
+        response: { total_posts, favorite_tag, warning_points }
       }
     } catch (error) {
       return {
