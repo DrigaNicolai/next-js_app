a1dcf575c0ddfef20b1b41ae95e06a3564b84d68
DrigaNicolai <just.whyme7@gmail.com>
2023-11-17 17:18:33 +0200

    Add/update statistics interfaces

--- ts/interface/statistic.ts
+++ ts/interface/statistic.ts
@@ -1,11 +1,17 @@
 export interface IGlobalStatistic {
   total_users: number;
   total_posts: number;
-  frequent_tags: Array<IFrequentTags>;
+  frequent_tags: Array<IFrequentTag>;
 }
 
-export interface IFrequentTags {
+export interface IFrequentTag {
   _id: string;
   tag_name: string;
   count: number;
+}
+
+export interface IUserStatistic {
+  total_posts: number;
+  favorite_tag: IFrequentTag;
+  warning_points: { total_points: number };
 }
